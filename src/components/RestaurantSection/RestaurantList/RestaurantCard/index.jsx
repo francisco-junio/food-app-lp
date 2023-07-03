68cd8d62d24b64f68bf5d758934e665e0c59bb28
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-07-03 10:46:35 -0300

    aula1

---
+++ src/components/RestaurantSection/RestaurantList/RestaurantCard/index.jsx
@@ -0,0 +1,9 @@
+export const RestaurantCard = () => {
+    return(
+        <li>
+            <img src="" alt="" />
+            <h3>Título e descrição</h3>
+            <p>Lorem ipsum dolor sit amet</p>
+        </li>
+    )
+}
---
+++ src/components/RestaurantSection/index.jsx
@@ -0,0 +1,10 @@
+import { RestaurantList } from "./RestaurantList"
+
+export const RestaurantSection = () => {
+    return(
+        <section>
+            <h2>Restaurantes famosos</h2>
+            <RestaurantList />
+        </section>
+    )
+}
