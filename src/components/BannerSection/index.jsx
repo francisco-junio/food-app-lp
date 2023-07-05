d76526a2c42eaf7cf7daff34c15e10d3c938f4ab
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-07-05 16:02:27 -0300

    feat: footer header and bannerSection styles

--- src/components/BannerSection/index.jsx
+++ src/components/BannerSection/index.jsx
@@ -1,23 +1,26 @@
 import BannerIllustration from "../../assets/BannerFood.png";
+import styles from "./style.module.css";
 
 export const BannerSection = () => {
    return (
-      <section className="section-padding">
+      <section className={`${styles.bannerSection} section-padding`}>
          <div className="container">
-            <div>
-               <h1 className="title black lg">
-                  Descubra o melhor <span className="red">aplicativo de comida</span>
-               </h1>
-               <p className="paragraph black">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin maximus
-                  tempor ullamcorper. Vivamus porta lorem eu quam ultricies, non imperdiet
-                  erat elementum.
-               </p>
-               <a href="#" className="btn red">
-                  Saiba mais
-               </a>
+            <div className={styles.flexBox}>
+               <div>
+                  <h1 className="title black lg">
+                     Descubra o melhor <span className="red">aplicativo de comida</span>
+                  </h1>
+                  <p className="paragraph black">
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+                     maximus tempor ullamcorper. Vivamus porta lorem eu quam ultricies,
+                     non imperdiet erat elementum.
+                  </p>
+                  <a href="#" className="btn red">
+                     Saiba mais
+                  </a>
+               </div>
+               <img src={BannerIllustration} alt="Comidas gostosas" />
             </div>
-            <img src={BannerIllustration} alt="Comidas gostosas" />
          </div>
       </section>
    );
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,8 +1,10 @@
+import styles from "./style.module.css"
+
 export const Footer = () => {
    return (
-      <footer>
+      <footer className={styles.footer}>
          <div className="container">
-            <p className="paragraph white">
+            <p className="paragraph white center">
                Todos os direitos reservados - Kenzie Academy Brasil - 2023{" "}
             </p>
          </div>
